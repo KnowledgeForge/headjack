48225467c436921833334271e485e60ca80e5b3a
circargs <nick@ouellet.dev>
2023-04-30 23:27:37 -0400

    working on ws session handling

--- demo.py
+++ demo.py
@@ -1,18 +1,28 @@
+from uuid import UUID, uuid4
 import uvicorn
-from fastapi import FastAPI, WebSocket
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.responses import HTMLResponse
 import logging
-
-from headjack_server.agents.standard import StandardAgent
+from headjack_server.models.utterance import Answer, User
+import asyncio
+from headjack_server.agents.standard import StandardAgent 
 from headjack_server.models.session import Session
 from headjack_server.tools.knowledge_search import KnowledgeSearchTool
+from fastapi.middleware.cors import CORSMiddleware
+import jwt
 
 _logger = logging.getLogger(__name__)
 
-from fastapi import Depends
-
-
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+SECRET_KEY = "secret"
 PORT = 8769
 html_template = f"""
 <!DOCTYPE html>
@@ -29,10 +39,16 @@ html_template = f"""
             <img src="https://avatars.githubusercontent.com/u/131468933?s=40&v=4" class="h-8 w-8 mr-2" alt="HeadJack Icon" />
             <h1 class="text-2xl font-bold">HeadJack Chat Client</h1>
         </div>
-        <div id="messages" class="bg-white rounded-lg shadow-lg p-4 mb-4" style="height: 500px; overflow-y: scroll;"></div>
-        <form id="form" class="flex items-center">
-            <input id="input" class="flex-grow border border-gray-400 px-4 py-2 rounded-lg mr-2 focus:outline-none focus:border-blue-500" autocomplete="off" autofocus/>
-            <button id="submit" class="bg-blue-500 text-white font-semibold px-4 py-2 rounded-lg">
+        <div class="flex justify-between items-center mb-4">
+            <button id="new_session" class="bg-blue-500 text-white font-semibold px-4 py-2 rounded-lg">New Session</button>
+            <label class="inline-flex items-center mt-3">
+                <input type="checkbox" class="form-checkbox h-5 w-5 text-gray-600" id="debug"><span class="ml-2">Debug</span>
+            </label>
+        </div>
+        <div id="messages" class="bg-white rounded-lg shadow-lg p-4 mb-4 disabled" style="height: 500px; overflow-y: scroll;"></div>
+        <form id="form" class="flex items-center disabled">
+            <input id="input" class="flex-grow border border-gray-400 px-4 py-2 rounded-lg mr-2 focus:outline-none focus:border-blue-500" autocomplete="off" autofocus disabled/>
+            <button id="submit" class="bg-blue-500 text-white font-semibold px-4 py-2 rounded-lg disabled">
                 <span id="submit-text">Send</span>
                 <div id="submit-loading" class="bg-blue-500 w-8 h-8 ml-2 rounded-full flex justify-center items-center hidden">
                     <div class="animate-spin rounded-full h-4 w-4 border-b-2 border-white"></div>
@@ -42,96 +58,147 @@ html_template = f"""
     </div>
     
     <script>
+        let ws;
+        let access_token;
+        
         const messages = document.getElementById('messages');
         const form = document.getElementById('form');
         const input = document.getElementById('input');
         const submitButton = document.getElementById('submit');
         const submitText = document.getElementById('submit-text');
         const submitLoading = document.getElementById('submit-loading');
+        const newSession = document.getElementById('new_session');
+        const debug = document.getElementById('debug');
+        
+        newSession.addEventListener('click', () => {{
+            fetch('/session')
+                .then(res => res.json())
+                .then(data => {{
+                    access_token = data.access_token;
+                    ws = new WebSocket(`ws://localhost:{PORT}/ws/${{access_token}}`);
+                    messages.classList.remove('disabled');
+                    form.classList.remove('disabled');
+                    input.disabled = false;
+                    submitButton.disabled = false;
+                    ws.onmessage = onMessage;
+                    newSession.style.display = 'none';
+                }})
+        }})
 
-        const ws = new WebSocket('ws://localhost:{PORT}/ws');
+        function onMessage(event) {{
+            const data = JSON.parse(event.data);
 
-        ws.onmessage = (event) => {{
-            const item = document.createElement('div');
-            if (event.data.startsWith("User:")) {{
-                item.classList.add('text-blue-500', 'font-semibold');
+            let item;
+            if (data.kind === 'User' || (debug.checked && data.kind !== 'Answer')) {{
+                item = document.createElement('div');
+                item.classList.add('text-right', 'bg-blue-100');
+                item.textContent = data.message;
+            }} else {{
+                item = document.createElement('div');
+                item.classList.add('text-left', 'bg-gray-100');
+                item.textContent = data.message;
             }}
-            item.textContent = event.data;
+            const info = document.createElement('div');
+            info.classList.add('text-xs', 'italic', 'text-gray-500');
+            info.textContent = `${{data.kind}} - ${{data.time}}`;
+            item.appendChild(info);
             messages.appendChild(item);
             messages.scrollTo(0, messages.scrollHeight);
-            console.log(event)
-            if (event.data == ""){{
-                input.disabled = false;
-                submitButton.disabled = false;
-                submitButton.classList.remove('opacity-50');
-                submitText.classList.remove('hidden');
-                submitLoading.classList.add('hidden');
-            }}
-        }};
+        }}
+        if (data.message !== "") {{
+            input.disabled = false;
+            submitButton.disabled = false;
+            submitButton.classList.remove('opacity-50');
+            submitText.classList.remove('hidden');
+            submitLoading.classList.add('hidden');
+        }}
+        
 
         form.onsubmit = async (event) => {{
             event.preventDefault();
+            if (!ws) return;
             input.disabled = true;
             submitButton.disabled = true;
             submitButton.classList.add('opacity-50');
             submitText.classList.add('hidden');
             submitLoading.classList.remove('hidden');
-            ws.send(input.value);
-            const item = document.createElement('div');
-            item.classList.add('text-right', 'italic', 'text-gray-500');
-            item.textContent = 'User: ' + input.value;
+            const message = input.value;
+            ws.send(JSON.stringify({{kind: "User", message}}));
+            let item = document.createElement('div');
+            item.classList.add('text-right', 'bg-blue-100');
+            item.textContent = message;
+            const info = document.createElement('div');
+            info.classList.add('text-xs', 'italic', 'text-gray-500');
+            info.textContent = `User - ${{new Date().toISOString()}}`;
+            item.appendChild(info);
             messages.appendChild(item);
             messages.scrollTo(0, messages.scrollHeight);
             input.value = '';
+            input.focus();
         }};
     </script>
 </body>
 </html>
 """
 
-
 @app.get("/")
-async def get():
-    return HTMLResponse(content=html_template)
+def get():
+    return HTMLResponse(html_template)
 
+@app.get("/session")
+def new_session():
+    access_token = jwt.encode({"session_id": str(uuid4())}, SECRET_KEY, algorithm="HS256")
+    return {"access_token": access_token}
 
-
-
-
-
-
-def get_agent_session():
+def get_agent_session(access_token: str):
+    payload = jwt.decode(access_token, SECRET_KEY, algorithms=["HS256"])
+    session_id = payload["session_id"]
+    session_uuid = UUID(session_id)
+    if session:=Session.sessions.get(session_uuid):
+        return session
     tools = [KnowledgeSearchTool()]
     agent = StandardAgent(
         model_identifier="chatgpt",
         tools=tools,
         decoder="argmax(openai_chunksize=4)",
     )
-    # agent = StandardAgent(
-    #     model_identifier="openai/text-ada-001",
-    #     tools=tools,
-    #     decoder="argmax(chatty_openai=True)",
-    # )
-    return Session(agent)
+    session = Session(agent, session_id=session_uuid)
+    return session
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: dict[str, WebSocket] = {}
+
+    async def connect(self, access_token: str, websocket: WebSocket):
+        session_id = jwt.decode(access_token, SECRET_KEY, algorithms=["HS256"])['session_id']
+        await websocket.accept()
+        self.active_connections[session_id]=websocket
+
+    def disconnect(self, websocket: WebSocket):
+        k= None
+        for k, v in self.active_connections.items():
+            if v == websocket:
+                break
+        if k is not None:
+            del self.active_connections[k]
 
 
-from headjack_server.models.utterance import Answer, User
-import asyncio
+manager = ConnectionManager()
 
-@app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket, *, session: Session = Depends(get_agent_session)):
-    await websocket.accept()
+@app.websocket("/ws/{access_token}")
+async def websocket_endpoint(websocket: WebSocket, access_token: str):
+    await manager.connect(access_token, websocket)
+    session = get_agent_session(access_token)
     while True:
-        data = await websocket.receive_text()
-        print(f"User message: {data}")
-        # await asyncio.sleep(2)
-        async for response in session(str(data)):
-            await websocket.send_text(str(response))
-        await websocket.send_text("")
-        # fin = asyncio.create_task(session.agent.run(User(str(data)), 3, {User, Answer}, 4, [], []))
-        # while not (fin.done() and session.agent.queue.empty()):
-        #     await websocket.send_text(str(await session.agent.queue.get()))
-
-
-if __name__ == "__main__":
+        try:
+            data = await websocket.receive_json()
+            message = data['message']
+            _logger.info(f"User message: {message}")
+            async for response in session(message):
+                await websocket.send_json({'message':str(response.utterance_), 'marker':response.marker, 'kind': response.__class__.__name__, "time": response.timestamp.strftime('%Y-%m-%d %H:%M:%S')})
+            await websocket.send_json({'message':"", "marker": "", "kind":"", "time":""})
+        except WebSocketDisconnect:
+            manager.disconnect(websocket)
+            
+if __name__ == "__main__":     
     uvicorn.run(app, host="0.0.0.0", port=PORT)
