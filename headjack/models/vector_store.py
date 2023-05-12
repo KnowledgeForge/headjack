b2f7b08e2be702c1691afd6507469a8fc19f4bc4
circargs <nick@ouellet.dev>
2023-05-12 12:38:57 -0400

    vector store steps towards tool example facilities

--- headjack/models/vector_store.py
+++ headjack/models/vector_store.py
@@ -1,15 +1,22 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from typing import List, Dict
 
 from chromadb.utils import embedding_functions
 from headjack.config import get_chroma_client
-
+from uuid import uuid4
 
 @dataclass
 class VectorStore:
-    collection_name: str
+    collection_name: str = field(default_factory=lambda: str(uuid4()))
 
     def __post_init__(self):
         ef = embedding_functions.SentenceTransformerEmbeddingFunction(model_name="all-MiniLM-L6-v2")
         self.client = get_chroma_client()
         self.collection = self.client.get_or_create_collection(self.collection_name, embedding_function=ef)
+
+    def add(self, documents: List[str], metadatas: List[Dict[str, str]], ids: List[str]):
+        self.collection.add(documents=documents, metadatas=metadatas, ids=ids)
+    
+    def query(self, query: str, n: int=1):
+        return self.collection.query(query_texts=query, n_results=n)
 
