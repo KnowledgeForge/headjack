efa50f79147c05871522112b7267be47a40f3963
Nick Ouellet <35903677+CircArgs@users.noreply.github.com>
2023-05-12 23:34:00 -0400

    Metric search agent (#15)
    * metric search begginings
    
    * metric search agent returns api results

--- headjack/agents/metric_search.py
+++ headjack/agents/metric_search.py
@@ -7,7 +7,7 @@ async def search_for_metrics(q):
     try:
         q = q.strip("\n '.")
         results = await fetch(f"{settings.search_service}/query?collection=metrics&text={q}", 'GET', return_json=True)
-        return [i for j in results['documents'] for i in j]
+        return results
     except:
         return "No results"
 
@@ -18,33 +18,31 @@ argmax
     """You are given some statement, terms or question below from the User.
     
     Here are some examples:
-    User: "What is the average temperature by city for the month of July?"
-    Terms: "average temperature", "mean temperature"
+    User: What is the average temperature by city for the month of July?
+    Terms: 'average temperature, mean temperature'
 
-    User: "total sales by region for the quarter ending September 2023?"
-    Terms: "total sales", "sales", "revenue"
+    User: total sales by region for the quarter ending September 2023?
+    Terms: 'total sales, sales, revenue'
     
-    User: "What is the total revenue for the company for the year 2022?"
-    Terms: "total revenue", "revenue", "sales"
+    User: What is the total revenue for the company for the year 2022?
+    Terms: 'total revenue, revenue, sales'
     
-    User: "What is the average number of employees by department?"
-    Terms: "average number of employees", "mean number of employees"
+    User: What is the average number of employees by department?
+    Terms: 'average number of employees, mean number of employees'
     
-    User:"What is the total number of flights by airline for the month of October?"
-    Terms: "total number of flights", "number of flights"
+    User: What is the total number of flights by airline for the month of October?
+    Terms: 'total number of flights, number of flights'
 
-    User: "avg rating for the restaurant by cuisine type"
-    Terms: "average rating", "mean rating"
+    User: avg rating for the restaurant by cuisine type
+    Terms: 'average rating, mean rating'
     
-    User: "registered users"
-    Terms: "total users", "number of users", "registered users"
+    User: registered users
+    Terms: 'total users, number of users, registered users'
     
     """
-    "Question: {question}\n"
-    "Action: Let's search for the terms '[TERM]\n"
-    result = await search_for_metrics(TERM)
-    "Result: {result}\n"
-    "Final Answer:[ANSWER]"
+    "User: {question}\n"
+    "Terms: '[TERM]\n"
+    return await search_for_metrics(TERM)
 FROM 
     "chatgpt"
 WHERE
---
+++ headjack/api/metric_search.py
@@ -0,0 +1,12 @@
+import logging
+
+from fastapi import APIRouter
+from starlette.responses import JSONResponse
+from headjack.agents.metric_search import metric_search_agent
+_logger = logging.getLogger(__name__)
+
+router = APIRouter(prefix="/metric_search", tags=["metric_search"])
+
+@router.post("/{query}")
+async def search_for_metrics(query: str)->JSONResponse:
+    return (await metric_search_agent(query))
