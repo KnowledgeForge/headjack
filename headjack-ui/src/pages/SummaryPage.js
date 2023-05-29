d16f3796d314eeff94e657e206f0e3a69bf186a0
samredai <sredai@netflix.com>
2023-05-29 14:06:02 -0400

    Sidebar, router, a few pages

---
+++ headjack-ui/src/pages/SummaryPage.js
@@ -0,0 +1,17 @@
+export default function SummaryPage() {
+    return (
+            <div className="container mx-auto mt-12">
+                <div className="grid grid-cols-1 gap-6 mb-6 lg:grid-cols-3">
+                    <div className="w-full px-4 py-5 bg-white rounded-lg shadow">
+                        <div className="text-sm font-medium text-gray-500 truncate">
+                            Documents
+                        </div>
+                        <div className="mt-1 text-3xl font-semibold text-gray-900">
+                            1,420
+                        </div>
+                    </div>
+                </div>
+            </div>
+    );
+  }
+  
