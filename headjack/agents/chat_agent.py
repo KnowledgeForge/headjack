e674176a3f6d1b259d2ed648c9d68709d87675e4
circargs <nick@ouellet.dev>
2023-06-09 12:33:42 -0400

    fixes to chat agent

--- headjack/agents/chat_agent.py
+++ headjack/agents/chat_agent.py
@@ -6,8 +6,8 @@ import lmql
 
 from headjack.agents.registry import AGENT_REGISTRY
 from headjack.models.utterance import Action, Answer, Utterance  # noqa: F401
-from headjack.utils.consistency import Consistency  # noqa: F401
-
+from headjack.utils.consistency import Consistency, consolidate_responses
+from headjack.utils.add_source_to_utterances import add_source_to_utterances
 _logger = logging.getLogger("uvicorn")
 
 
@@ -15,17 +15,6 @@ dispatchable_agents = indent(
     dedent("\n".join([f"{agent_name}: {agent_description}" for agent_name, (agent_description, _) in AGENT_REGISTRY.items()])),
     " " * 4,
 )
-
-
-async def chat_agent(
-    question: Utterance,
-    max_steps: int = 3,
-    chat_consistency: Consistency = Consistency.OFF,
-    agent_consistency: Consistency = Consistency.OFF,
-) -> Utterance:
-    return await _chat_agent(
-        ChatAgentArgs(question, max_steps, *Consistency.map(chat_consistency), *Consistency.map(agent_consistency)),
-    )
 
 
 @dataclass
@@ -38,10 +27,19 @@ class ChatAgentArgs:
     agent_temp: float
 
 
+async def chat_agent(
+    question: Utterance,
+    max_steps: int = 3,
+    chat_consistency: Consistency = Consistency.OFF,
+    agent_consistency: Consistency = Consistency.OFF,
+) -> Utterance:
+    return await consolidate_responses(add_source_to_utterances(await _chat_agent(ChatAgentArgs(question, max_steps, *Consistency.map(chat_consistency), *Consistency.map(agent_consistency))), 'chat_agent'))
+
+
 @lmql.query
-async def _chat_agent(args: ChatAgentArgs) -> Utterance:  # type: ignore
+async def _chat_agent(input: ChatAgentArgs) -> Utterance:  # type: ignore
     '''lmql
-    sample(n = n, temperature = temp)
+    sample(n = input.n, temperature = input.temp, max_len=4096)
         """You are an chatbot that takes a conversation between you and a User and continues the conversation appropriately.
         To aid you in responding to the user, you have access to several helpful specialist agents that can help with tasks or questions you dispatch to them.
 
@@ -49,11 +47,12 @@ async def _chat_agent(args: ChatAgentArgs) -> Utterance:  # type: ignore
         {dispatchable_agents}
 
         Conversation:
-        {dedent(args.question.convo())}
+        {dedent(input.question.convo())}
         """
+
         steps = 0
-        while args.max_steps>steps:
-            "Is there information available in the existing conversation that can be used to respond? Yes or No.: [CONVO_INFO]\n"
+        while input.max_steps>steps:
+            "Is there information available in above that can be used to respond? Yes or No.: [CONVO_INFO]\n"
             if CONVO_INFO=='No':
                 """Do you need help from a specialist to continue or can you respond immediately based on information from the existing conversation?
                 Yes for specialist otherwise No.: [SPECIALIST]
@@ -62,12 +61,14 @@ async def _chat_agent(args: ChatAgentArgs) -> Utterance:  # type: ignore
                 steps+=1
                 """The agent that seems best suited to handle this request is: [AGENT]
                 What is the question or task this specialist should assist you with?
-                Write your request in the task xml tags below e.g. <task>your task description or question here</task>. Your request should be as terse as possible, most likely less than 100 words.
+                Write your request in the task xml tags below e.g. <task>your task description or question here</task>. 
+                Your request should be as terse as possible, most likely less than 100 words.
+                Do not add anything to your task request that is not derived from above.
                 <task>[TASK]task>
                 """
-                task = Action(utterance=TASK.strip('</'), parent_=args.question)
+                task = Action(utterance=TASK.strip('</'), parent_=input.question)
                 _logger.info(f"Chat agent dispatching to {AGENT} for task `{task}`.")
-                result = (await AGENT_REGISTRY[AGENT][1](task, args.agent_n, args.agent_temp))
+                result = (await AGENT_REGISTRY[AGENT][1](task, input.agent_n, input.agent_temp))
                 "Is the result of this specialist likely a response to the user? Yes or No.: [IS_DIRECT]"
                 if IS_DIRECT=='Yes':
                     return result
@@ -77,18 +78,15 @@ async def _chat_agent(args: ChatAgentArgs) -> Utterance:  # type: ignore
                     if IS_DIRECT=='Yes':
                         return result
             else:
-                """Respond to the user in a few words (less than 200) using information directly available to you in this conversation.
+                """Respond to the user in a few words (preferably less than 200) using information directly available to you in this conversation.
                 Answer: [ANSWER]"""
-                return Answer(utterance=ANSWER, parent_=args.question)
+                return Answer(utterance=ANSWER, parent_=input.question)
     from
         "chatgpt"
     where
         AGENT in [agent for agent in AGENT_REGISTRY.keys()] and
         SPECIALIST in ['Yes', 'No'] and
         CONVO_INFO in ['Yes', 'No'] and
-        SPECIALIST in ['Yes', 'No'] and
-        CONVO_INFO in ['Yes', 'No'] and
         IS_DIRECT in ['Yes', 'No'] and
-        STOPS_AT(TASK, '</') and
-        STOPS_AT(ANSWER, '\n')
+        STOPS_AT(TASK, '</')
     '''
